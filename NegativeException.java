9cbd362709ef58a3b8c5ec8a579f5a5f75cd5cd3
reshetha <42463874+reshetha@users.noreply.github.com>
2018-08-29 13:01:50 +0000

    Add files via upload

---
+++ NegativeException.java
@@ -0,0 +1,11 @@
+package com.capgemini.day5.Exception;
+
+public class NegativeException extends Exception 
+{
+	public NegativeException(String message)
+	{
+		super(message);
+	}
+	
+
+}
---
+++ ZeroException.java
@@ -0,0 +1,10 @@
+package com.capgemini.day5.Exception;
+
+public class ZeroException extends Exception
+{
+	public ZeroException(String message)
+	{
+		super(message);
+	}
+
+}
